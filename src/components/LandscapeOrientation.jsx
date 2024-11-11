7b850ab3bac6084a4dd172fe03e0bd27d844bba8
Greg <greg@MacBook-Pro.local>
2024-11-10 20:47:00 -0500

    DP-42 Fixed The Logo Issue

--- src/components/LandscapeOrientation.jsx
+++ src/components/LandscapeOrientation.jsx
@@ -4,9 +4,9 @@ import logo from '../assets/logo.png';
 export default function LandscapeOrientation({children}) {
   return (
     <div>
-      <div className="landscape-message flex flex-cols items-center justify-center w-full">
-      <div className='flex items-center justify-center'><img src={logo} className='mb-4 w-5 md:w-10' /><div className='text-[blue] md:text-2xl ml-2'><span className='text-[green]'>WaterWatch</span><span className='font-bold'>PRO&trade;</span></div></div>
-        <p>Please rotate your device to portrait mode.</p>
+      <div className="landscape-message hidden flex flex-col items-center justify-center w-full gap-4">
+      <div className='flex items-center justify-center'><img src={logo} className='mb-4 w-5 md:w-[20rem]' /></div>
+        <p className='pl-4'>Please rotate your device to portrait mode.</p>
       </div>
       <div className="app-content">
         {children}

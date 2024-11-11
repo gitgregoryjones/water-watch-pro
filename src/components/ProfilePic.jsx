884d86ad5794488e3868fa1c0f8f3b20dc2362c6
Greg <greg@MacBook-Pro.local>
2024-11-10 22:28:41 -0500

    DP-41 Add First Initial and Last Initial to Profile

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -12,11 +12,12 @@ export default function ProfilePic(className){
     const user = useSelector( state => state.userInfo.user)
 
     return (
-            <div className={`hidden md:flex text-sm  tab ${className}`}>
+            <div className={`flex md:flex text-sm items-end justify-center tab ${className}`}>
         
-        {user.name} ({user.role})
+        <div className='hidden'>{user.name} ({user.role})</div>
+        <div title={`${user.name} ${user.role}`} className='cursor-pointer text-md bg-[#217e3f] text-[white] p-2 rounded-[50%]'>{user.firstName[0]} {user.lastName[0]}</div>
         <Upgrade  showMsg={false}>
-        <i className="hidden md:block pl-2 text-slate-400 text-sm  fa-solid fa-user-group"></i>
+        
         &nbsp; <Link to="/double">Switch User</Link>
         </Upgrade>
         

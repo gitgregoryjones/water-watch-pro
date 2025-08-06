5fb265752e965c479356d1f0c84a2373c0ef6f0d
Greg <greg@MacBook-Pro.local>
2025-08-06 13:12:56 -0400

    fixed branch

--- src/lib/use-feature-flags.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export function useFeatureFlags() {
-  const [loading] = useState(false);
-  const isActive = (name) => ['dark-mode', 'https://blinkprojects.atlassian.net/browse/DP-197'].includes(name);
-  return { isActive, loading };
-}

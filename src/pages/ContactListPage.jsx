0ddcedd7e542ca00540d1f2df148e93ee345bf98
Greg <greg@MacBook-Pro.local>
2025-08-08 07:58:33 -0400

    chore: remove stale feature flags from src/pages/ContactListPage.jsx

--- src/pages/ContactListPage.jsx
+++ src/pages/ContactListPage.jsx
@@ -36,9 +36,8 @@ const ContactListPage = () => {
 
       let url = `/api/contacts/?client_id=${user.clients[0].id}`
       
-      if(user.role == "admin"){
-        url =  isActive("unarchive-contacts") ? `/api/contacts/all/?a=true&status=all` : `/api/contacts/all/?a=true`;
-
+if(user.role == "admin"){
+        url =  `/api/contacts/all/?a=true&status=all`;
       }
      
       let rows = await fetchByPage(url)

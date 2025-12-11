f67c25b0ebe3725dd828248603df3707acad54ea
Greg <greg@MacBook-Pro.local>
2025-12-10 19:28:31 -0500

    fix page

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -121,17 +121,17 @@ const UserForm = ({ clientToEdit, myself }) => {
   };
 
   return (
-    <div className="h-full w-full flex flex-col mt-28">
-      <h1 className="text-2xl font-bold text-green-800 m-8 self-start">
+    <div className="h-full w-full flex flex-col items-center  mt-18 text-gray-900">
+      <h1 className="text-2xl font-bold text-green-800 dark:text-green-200 m-8 self-start">
         User Profile &gt;{firstName} {lastName}
       </h1>
 
       <Card
         
-        className="border-[whitesmoke] bg-[whitesmoke] md:rounded-[unset]"
+        className=" mx-auto    shadow-md  rounded-lg"
       >
-        <div className="p-6 w-full md:w-full mx-auto bg-white shadow-md rounded-lg">
-          <div className="relative mt-0 p-6 w-full mx-auto bg-white rounded-lg">
+        <div className=" w-full md:w-full mx-auto  rounded-lg">
+          <div className="relative mt-0 w-full mx-auto rounded-lg">
             
 
             <h1 className="text-2xl font-bold mb-4">Edit User {firstName}</h1>
@@ -140,9 +140,9 @@ const UserForm = ({ clientToEdit, myself }) => {
               <div id="msg">{msg}</div>
 
               {/* First Name */}
-              <div className="flex flex-col shadow rounded border p-4">
+              <div className="flex flex-col gap-4 shadow rounded  p-4">
                 <div>
-                  <label htmlFor="firstName" className="block text-gray-700 font-bold">
+                  <label htmlFor="firstName" className="block py-2  font-bold">
                     First Name
                   </label>
                   <input
@@ -157,7 +157,7 @@ const UserForm = ({ clientToEdit, myself }) => {
 
                 {/* Last Name */}
                 <div>
-                  <label htmlFor="lastName" className="block text-gray-700 font-bold">
+                  <label htmlFor="lastName" className="block py-2  font-bold">
                     Last Name
                   </label>
                   <input
@@ -165,14 +165,15 @@ const UserForm = ({ clientToEdit, myself }) => {
                     type="text"
                     value={lastName}
                     onChange={(e) => setLastName(e.target.value)}
-                    className="border border-gray-300 rounded p-2 w-full"
+                                        className="border border-gray-300 rounded p-2 w-full"
+
                     required
                   />
                 </div>
 
                 {/* Email */}
                 <div>
-                  <label htmlFor="email" className="block text-gray-700 font-bold">
+                  <label htmlFor="email" className="block py-2 font-bold">
                     Email
                   </label>
                   <input
@@ -187,7 +188,7 @@ const UserForm = ({ clientToEdit, myself }) => {
 
                 {/* Phone */}
                 <div>
-                  <label htmlFor="phone" className="block text-gray-700 font-bold">
+                  <label htmlFor="phone" className="block py-2 font-bold">
                     Phone
                   </label>
                   <input
@@ -201,7 +202,7 @@ const UserForm = ({ clientToEdit, myself }) => {
                 </div>
               </div>
 
-              <div className="flex flex-col shadow rounded border p-4 gap-4">
+              <div className="flex flex-col shadow rounded  p-4 gap-4">
                 <h2 className="text-xl font-bold">Notification Settings</h2>
 
                 <div className="grid grid-cols-2 gap-4 border-t pt-4">

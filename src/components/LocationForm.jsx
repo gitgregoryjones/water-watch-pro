c55c36335860eff93d7cb59fe5927fa6c0f46946
gitgregoryjones <gregory.jones@synovas.com>
2026-08-24 20:35:22 -0400

    Expand valid latitude range to 49 degrees

--- src/components/LocationForm.jsx
+++ src/components/LocationForm.jsx
@@ -130,9 +130,9 @@ const LocationForm = ({ locationToEdit = null, onSubmitSuccess }) => {
 
        
 
-        if(!latitude || latitude < 24 || latitude > 48 ){
+        if(!latitude || latitude < 24 || latitude > 49 ){
             
-             setMsg(<span className="text-[red]">Latitude must be between 24 and 48 degrees</span>)
+             setMsg(<span className="text-[red]">Latitude must be between 24 and 49 degrees</span>)
              setIsWorking(false); 
             success = false;
         }
